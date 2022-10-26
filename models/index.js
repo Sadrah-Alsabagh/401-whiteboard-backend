a065d13d0705d4b2d023a63121305fd4025ec071
Sadrah-Alsabagh <sedrasabbagh33@gmail.com>
2022-10-26 20:15:55 +0000

    create post model

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// const { module } = require('modules');
+//const { module } = require('modules');
 const{Sequelize, DataTypes} = require('sequelize'); 
 
-const POSTGRES_URL = process.env.DATABASE_URL;
+const POSTGRES_URL = "postgres://sedra:sedra@123@127.0.0.1:5432/whiteboard";
 
 let sequelize = new Sequelize(POSTGRES_URL);
 
